c58f4a1344cf3e0faec44004f0107df4506e86fc
Jonathan Grim <jonjongrim@gmail.com>
2017-09-13 17:26:00 -0400

    Add IGDB api routes

--- api/game.js
+++ api/game.js
@@ -1,5 +1,31 @@
 const Game = require('../models').Game;
 const router = require('express').Router();
+const igdb = require('igdb-api-node').default;
+
+const client = igdb(process.env.IGDB_KEY);
+
+function searchForGame(title) {
+  return client
+    .games({
+      fields: '*',
+      search: title
+    })
+    .then(response => {
+      return response.body;
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+
+function getGameLogoImage(cloudinary_id) {
+  return client.image({ cloudinary_id }, 'logo_med', 'png');
+}
+
+async function getFirstGameFound(title) {
+  const gameArray = await searchForGame(title);
+  return gameArray[0];
+}
 
 router
   .route('/')
@@ -32,7 +58,17 @@ router.route('/:id').get(function(req, res) {
   Game.findById(req.params.id)
     .then(result => {
       res.setHeader('Access-Control-Allow-Origin', '*');
-      res.json(result);
+      const gamePromise = getFirstGameFound(result.title);
+      gamePromise
+        .then(game => {
+          if (game.cover.cloudinary_id) {
+            game.imageUrl = getGameLogoImage(game.cover.cloudinary_id);
+          }
+          res.json(game);
+        })
+        .catch(err => {
+          throw err;
+        });
     })
     .catch(err => {
       console.log(err);
